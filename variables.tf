e816dc2fd22114f10087872ac39b89786e0cfa73
praveenvavilla-git <67928454+praveenvavilla-git@users.noreply.github.com>
2022-08-11 19:45:14 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "praveenawskey"
+  default     =  "p**************key"
 }
 
 variable "instance_type" {
